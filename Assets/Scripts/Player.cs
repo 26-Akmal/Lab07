3b9f280db3f0a45c3cb8e8db16ff0e00669849be
SAMDOL <akmal5000sayhello@gmail.com>
2022-10-25 10:59:14 +0800

    Unity Start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private float movementSpeed = 3f;
+    private Rigidbody rb;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * movementSpeed;
             thisAnimation.Play();
+        }
+            
     }
 }
